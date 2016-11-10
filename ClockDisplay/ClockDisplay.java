f781404ade1b25943209007060ce125ed9e760fc
alejandro <alejandro>
2016-11-10 11:55:29 +0100

    Creo un método que permita cambiar la hora

---
+++ ClockDisplay/ClockDisplay.java
@@ -0,0 +1,25 @@
+
+
+public class ClockDisplay
+{
+    
+    private NumberDisplay hora;
+    
+    private NumberDisplay minuto;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        hora = new NumberDisplay(24);
+        minuto = new NumberDisplay(60);
+    }
+    public void fijarHora(int nuevaHora, int nuevoMinuto)
+    {
+        hora.setValue(nuevaHora);
+        minuto.setValue(nuevoMinuto);
+    }
+
+    
+}
